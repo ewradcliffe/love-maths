ed0666ecd9499183e6314e29ed62e63f3b6be323
Ed Radcliffe <ewradcliffe@yahoo.com>
2024-05-17 10:32:59 +0000

    add code to check answer and respond to user.

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
     for (button of buttons) {
         button.addEventListener('click', function() {
             if (this.getAttribute('data-type') === 'submit') {
-                alert('You clicked Submit!')
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute('data-type');
                 runGame(gameType);
@@ -36,8 +36,21 @@ function runGame(gameType) {
     }  
 }
 
-function checkAnswer() {
+/**
+ * Checs the answer against the first element in 
+ * the returned calculateCorrectAnswer array.
+ */
 
+function checkAnswer() {
+    let userAnswer = parseInt(document.getElementById('answer-box').value);
+    let calculatedAnswer = caculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+
+    if (isCorrect) {
+        alert('Hey! You got it right! :D');
+    } else {
+        alert(`Awwww..... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+    }
 }
 
 /**
