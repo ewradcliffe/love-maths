2a9f034a1872a9be775421263d6312bc1c2582ec
Ed Radcliffe <ewradcliffe@yahoo.com>
2024-05-17 10:58:23 +0000

    added functions to track score.

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,8 +48,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert('Hey! You got it right! :D');
+        incrementScore();
     } else {
         alert(`Awwww..... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 }
 
@@ -71,12 +73,20 @@ function caculateCorrectAnswer() {
 
 }
 
+/**
+ * Gets the current score from the DOM and increments it by 1.
+ */
 function incrementScore() {
-
+    let oldScore = parseInt(document.getElementById('score').innerText)
+    document.getElementById('score').innerText = ++oldScore;
 }
 
+/**
+ * Gets the current incorrect score from the DOM and increments it by 1.
+ */
 function incrementWrongAnswer() {
-
+    let oldScore = parseInt(document.getElementById('incorrect').innerText)
+    document.getElementById('incorrect').innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
